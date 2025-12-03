9c59ead3e1b69d3611e7ad19b1622813cefb0a01
Joseph Pasia <joseph.pasia@gmail.com>
2025-12-03 08:33:04 +0100

    remove old subfolders and their contents

--- src/features/build_features.py
+++
@@ -1,3 +0,0 @@
-def add_features(df):
-    df['TotalSF'] = df['TotalBsmtSF'] + df['1stFlrSF'] + df['2ndFlrSF']
-    return df
--- src/models/predict.py
+++
@@ -1,4 +0,0 @@
-import tensorflow as tf
-def predict(model_path, X):
-    model = tf.keras.models.load_model(model_path)
-    return model.predict(X)

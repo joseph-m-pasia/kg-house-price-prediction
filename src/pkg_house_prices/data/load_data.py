5b63e28f0cc8f6912480525ee15e6f2d73a05e44
Joseph Pasia <joseph.pasia@gmail.com>
2025-12-05 22:29:02 +0100

    added the CONFIG variable plus the updated paths

--- src/pkg_house_prices/data/load_data.py
+++ src/pkg_house_prices/data/load_data.py
@@ -1,6 +1,13 @@
 import pandas as pd
+from pathlib import Path
+from pkg_house_prices.utils.config import CONFIG  # import the shared CONFIG
+
 
 def load_data(train_path, test_path):
+
+    train_path = Path(CONFIG["data"]["train"])
+    test_path  = Path(CONFIG["data"]["test"])
+
     train = pd.read_csv(train_path)
     test = pd.read_csv(test_path)
     return train, test

ce55f3fc34e467e2286fb267b8362fadcabb946c
Joseph Pasia <joseph.pasia@gmail.com>
2025-12-05 23:35:31 +0100

    import the pandas package

--- src/pkg_house_prices/utils/config.py
+++ src/pkg_house_prices/utils/config.py
@@ -6,8 +6,7 @@ def _load_config():
     Loads the YAML configuration file from project root
     """
     # Path relative to this file: go up two levels to project root
-    config_path = Path(__file__).parents[2] / "config.yaml"
-
+    config_path = Path(__file__).parents[3] / "config.yaml"
     with open(config_path, "r") as f:
         return yaml.safe_load(f)
      

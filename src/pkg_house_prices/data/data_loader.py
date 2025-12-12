8466b12c225851333e3c0bc49fcab27f411dfec7
Joseph Pasia <joseph.pasia@gmail.com>
2025-12-12 23:14:36 +0100

    simplify the loading of data

--- src/pkg_house_prices/data/data_loader.py
+++ src/pkg_house_prices/data/data_loader.py
@@ -5,28 +5,31 @@ from pkg_house_prices.utils.logger import logger
 from pkg_house_prices.utils.read_config import read_config 
 
 # --- Load data function ---
-def load_data():
+def _load_data():
     """
     Load train and test datasets using absolute paths from config
     """
-    logger.info("Executing load_data():")
+    logger.info("_load_data() - Executing load data function...")
    
     # Logging paths
     train_path = read_config("data", "train")
     test_path  = read_config("data", "test")
 
-    logger.info(f"Loading train data from: {train_path}")
-    logger.info(f"Loading test data from: {test_path}")
+    logger.info(f"_load_data() - Loading train data from {train_path}")
+    logger.info(f"_load_data() - Loading test data from {test_path}")
 
    # Optional sanity check
     if not train_path.exists():
-        logger.error(f"Train file not found: {train_path}")
+        logger.error(f"_load_data() - Train file not found: {train_path}")
         raise FileNotFoundError(f"Train file not found: {train_path}")
     if not test_path.exists():
-        logger.error(f"Test file not found: {test_path}")
+        logger.error(f"_load_data() - Test file not found: {test_path}")
         raise FileNotFoundError(f"Test file not found: {test_path}")
 
     train = pd.read_csv(train_path)
     test  = pd.read_csv(test_path)
 
     return train, test
+
+train, test = _load_data()
+

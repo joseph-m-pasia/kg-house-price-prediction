b8e319ddba22974bcceccaba6ba0062253cb8197
Joseph Pasia <joseph.pasia@gmail.com>
2025-12-12 23:00:44 +0100

    minor changes

--- src/pkg_house_prices/data/data_loader.py
+++ src/pkg_house_prices/data/data_loader.py
@@ -1,12 +1,32 @@
 from pathlib import Path
 import pandas as pd
-from pkg_house_prices.utils.config import CONFIG
+from pkg_house_prices.utils.config import CONFIG  # your YAML loader
+from pkg_house_prices.utils.logger import logger
+from pkg_house_prices.utils.read_config import read_config 
 
-def load_data(CONFIG):
+# --- Load data function ---
+def load_data():
+    """
+    Load train and test datasets using absolute paths from config
+    """
+    logger.info("Executing load_data():")
+   
+    # Logging paths
+    train_path = read_config("data", "train")
+    test_path  = read_config("data", "test")
 
-    train_path = Path(CONFIG["data"]["train"])
-    test_path  = Path(CONFIG["data"]["test"])
+    logger.info(f"Loading train data from: {train_path}")
+    logger.info(f"Loading test data from: {test_path}")
+
+   # Optional sanity check
+    if not train_path.exists():
+        logger.error(f"Train file not found: {train_path}")
+        raise FileNotFoundError(f"Train file not found: {train_path}")
+    if not test_path.exists():
+        logger.error(f"Test file not found: {test_path}")
+        raise FileNotFoundError(f"Test file not found: {test_path}")
 
     train = pd.read_csv(train_path)
-    test = pd.read_csv(test_path)
+    test  = pd.read_csv(test_path)
+
     return train, test
--- src/pkg_house_prices/utils/config.py
+++ src/pkg_house_prices/utils/config.py
@@ -1,13 +1,15 @@
 import yaml
 from pathlib import Path
+from pkg_house_prices.utils.logger import logger
 
 def _load_config():
     """
     Loads the YAML configuration file from project root
     """
-    # Path relative to this file: go up two levels to project root
+    # Path relative to this file: go up three levels to project root (house_prices_tensorflow)
     config_path = Path(__file__).parents[3] / "config.yaml"
     with open(config_path, "r") as f:
         return yaml.safe_load(f)
      
 CONFIG = _load_config()
+

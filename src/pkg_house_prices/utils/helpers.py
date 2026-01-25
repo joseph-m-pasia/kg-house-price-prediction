8dfeed8ec6a488e1bd82c3f7ee9f018f1afb8832
Joseph Pasia <joseph.pasia@gmail.com>
2026-01-25 21:31:07 +0100

    fea: Added the function read_config

--- src/pkg_house_prices/utils/helpers.py
+++ src/pkg_house_prices/utils/helpers.py
@@ -1,7 +1,8 @@
 import joblib
 import os
 from pkg_house_prices.utils.logger import logger
-
+from pkg_house_prices.utils.project_root import PROJECT_ROOT
+from pkg_house_prices.utils.config import CONFIG
 
 # Load the model
 def read_joblib(path, filename):
@@ -10,3 +11,15 @@ def read_joblib(path, filename):
     model = joblib.load(full_path)
     return model
 
+def read_config(*keys):
+    """
+    Fetch nested YAML keys and return the values
+    Example: read_config("data", "train")
+    """
+    logger.info(f"read_config() - Reading config keys: {keys}")
+    logger.info(f"read_config() - Project Root is {PROJECT_ROOT}")
+
+    d = CONFIG
+    for k in keys:
+        d = d[k]
+    return PROJECT_ROOT / d

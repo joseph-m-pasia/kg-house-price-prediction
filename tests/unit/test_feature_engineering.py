b03e7b11dcb7e7af0611b82bfce966b8a43b164a
Joseph Pasia <joseph.pasia@gmail.com>
2026-07-18 16:31:26 +0200

    fix the issue wiht black

--- tests/unit/test_feature_engineering.py
+++ tests/unit/test_feature_engineering.py
@@ -9,6 +9,7 @@ def test_fit_returns_self(sample_df):
 
     assert fe.fit(sample_df) is fe
 
+
 # 2. HouseAge is computed correctly
 def test_house_age_created(sample_df):
     fe = FeatureEngineer()
@@ -23,6 +24,7 @@ def test_house_age_created(sample_df):
         expected,
         check_names=False,
     )
+
 
 # 3. RemodAge is computed correctly
 def test_remod_age_created(sample_df):
@@ -39,17 +41,14 @@ def test_remod_age_created(sample_df):
         check_names=False,
     )
 
+
 # 4. TotalSF is correct
 def test_total_sf_created(sample_df):
     fe = FeatureEngineer()
 
     result = fe.transform(sample_df)
 
-    expected = (
-        sample_df["TotalBsmtSF"]
-        + sample_df["1stFlrSF"]
-        + sample_df["2ndFlrSF"]
-    )
+    expected = sample_df["TotalBsmtSF"] + sample_df["1stFlrSF"] + sample_df["2ndFlrSF"]
 
     assert "TotalSF" in result.columns
     pd.testing.assert_series_equal(
@@ -57,6 +56,7 @@ def test_total_sf_created(sample_df):
         expected,
         check_names=False,
     )
+
 
 # 5. TotalBathrooms is correct
 def test_total_bathrooms_created(sample_df):
@@ -78,43 +78,39 @@ def test_total_bathrooms_created(sample_df):
         check_names=False,
     )
 
+
 # 6. Missing values are filled with zero
 def test_missing_numeric_filled():
-    df = pd.DataFrame({
-        "TotalBsmtSF":[None],
-        "1stFlrSF":[900],
-        "2ndFlrSF":[0]
-    })
+    df = pd.DataFrame({"TotalBsmtSF": [None], "1stFlrSF": [900], "2ndFlrSF": [0]})
 
     fe = FeatureEngineer()
 
     result = fe.transform(df)
 
-    assert result.loc[0,"TotalBsmtSF"] == 0
+    assert result.loc[0, "TotalBsmtSF"] == 0
+
 
 # 7. HasBasement
 def test_has_basement():
-    df = pd.DataFrame({
-        "TotalBsmtSF":[0,500]
-    })
+    df = pd.DataFrame({"TotalBsmtSF": [0, 500]})
 
     fe = FeatureEngineer()
 
     result = fe.transform(df)
 
-    assert result["HasBasement"].tolist() == [0,1]
+    assert result["HasBasement"].tolist() == [0, 1]
+
 
 # 9. HasPool
 def test_has_pool():
-    df = pd.DataFrame({
-        "PoolArea":[0,100]
-    })
+    df = pd.DataFrame({"PoolArea": [0, 100]})
 
     fe = FeatureEngineer()
 
     result = fe.transform(df)
 
-    assert result["HasPool"].tolist() == [0,1]
+    assert result["HasPool"].tolist() == [0, 1]
+
 
 # 11. Original dataframe is unchanged
 def test_original_dataframe_not_modified(sample_df):
@@ -123,5 +119,3 @@ def test_original_dataframe_not_modified(sample_df):
     FeatureEngineer().transform(sample_df)
 
     pd.testing.assert_frame_equal(sample_df, original)
-
-    

4b536a10b8779174e52f4e62f4904a16b840577d
Joseph Pasia <joseph.pasia@gmail.com>
2026-08-01 23:08:55 +0200

    refactor: remove model_loader.py as it is no longer needed

--- app/model_loader.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import joblib
-
-MODEL_PATH = Path(__file__).parents[1] / "artifacts" / "trained_models" / "xgboost_regression.joblib"
-
-
-def get_pipeline():
-    """Load the trained pipeline."""
-    return joblib.load(MODEL_PATH)
